e315bee675d6a4903afddc53dd40d8542a961fbe
Merwan-J <merwanj71@gmail.com>
2023-03-11 19:46:07 +0300

    [api] implemented ranking

--- API/src/users/services/users/users.service.ts
+++ API/src/users/services/users/users.service.ts
@@ -11,6 +11,13 @@ export class UsersService {
   async createUser(createDto: createUserDto): Promise<IUser> {
     try {
       const user: IUser = await this.userModel.create(createDto);
+      const { lastRank, users } = await this.getAllRank();
+
+      const lastUserRank: number = users.at(-1)['rank'];
+      const lastUserPoint: number = users.at(-1)['points'];
+
+      user.rank = lastUserPoint === 0 ? lastUserRank : lastRank + 1;
+      await user.save();
       return user;
     } catch (err) {
       throw err;
@@ -47,6 +54,16 @@ export class UsersService {
       throw err;
     }
   }
+
+  async isUserAdmin(id: string) {
+    try {
+      const user: IUser = await this.userModel.findById(id);
+      return user.role === 'admin';
+    } catch (err) {
+      throw err;
+    }
+  }
+
   async delete(id: string) {
     try {
       await this.userModel.findOneAndDelete({ telegramId: id });
@@ -54,4 +71,60 @@ export class UsersService {
       throw new BadRequestException(err);
     }
   }
+
+  async getAllRank() {
+    try {
+      const users = await this.userModel.find().sort({ points: -1 });
+
+      let rankArray = [];
+      for (let i = 0; i < users.length; i++) {
+        rankArray.push(users[i]['points']);
+      }
+
+      const lastRank = await this.giveRank(rankArray, users);
+      return { lastRank, users };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async giveRank(arrayArg, resultArg) {
+    let rank = 1;
+    let prev_rank = rank;
+    let position = 0;
+    // displaying the headers in the console
+
+    // looping through the rank array
+    for (let i = 0; i < arrayArg.length; i++) {
+      /*
+      If it is the first index, then automatically the position becomes 1.
+      */
+      if (i == 0) {
+        position = rank;
+
+        /*
+      if the value contained in `[i]` is not equal to `[i-1]`, increment the `rank` value and assign it to `position`.
+      The `prev_rank` is assigned the `rank` value.
+      */
+      } else if (arrayArg[i] != arrayArg[i - 1]) {
+        rank++;
+        position = rank;
+        prev_rank = rank;
+
+        /*
+      Otherwise, if the value contained in `[i]` is equal to `[i-1]`,
+      assign the position the value stored in the `prev_rank` variable then increment the value stored in the `rank` variable.*/
+      } else {
+        position = prev_rank;
+        rank++;
+      }
+
+      const user: IUser = resultArg[i];
+      user.rank = position;
+      await user.save();
+    }
+
+    return rank;
+  }
+  async getRankById() {}
 }
